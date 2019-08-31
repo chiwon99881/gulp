df808090e9f877d74901255938a2fbea075d5387
Chiwon <chiwon99881@gmail.com>
2019-08-31 13:38:43 +0900

    build and deploy

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # GulpJS
 
 -Learn Gulp by building an awesome development environment
+
+## deploy pages
+
+- https://chiwon99881.github.io/gulp/
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,6 +8,7 @@ import autoprefixer from "gulp-autoprefixer";
 import miniCSS from "gulp-csso";
 import bro from "gulp-bro";
 import babelify from "babelify";
+import ghPages from "gulp-gh-pages";
 
 sass.compiler = require("node-sass");
 
@@ -39,7 +40,7 @@ const pug = () =>
     .pipe(gpug())
     .pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del(["build"]);
+const clean = () => del(["build", ".publish"]);
 const webserver = () =>
   gulp.src("build").pipe(ws({ livereload: true, open: true }));
 
@@ -74,6 +75,8 @@ const js = () =>
     )
     .pipe(gulp.dest(routes.js.dest));
 
+const ghDeploy = () => gulp.src("build/**/*").pipe(ghPages());
+
 const watch = () => {
   gulp.watch(routes.pug.watch, pug);
   gulp.watch(routes.img.src, img);
@@ -88,3 +91,5 @@ const assets = gulp.series([pug, styles, js]);
 const postDev = gulp.parallel([webserver, watch]);
 
 export const dev = gulp.series([prepare, assets, postDev]);
+export const build = gulp.series([prepare, assets]);
+export const deploy = gulp.series([build, ghDeploy, clean]);
